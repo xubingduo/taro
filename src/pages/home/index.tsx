5181755458c0e9bf789f78d1e5673dfd9e440104
chen <13454735035@163.com>
2020-03-16 16:37:54 +0800

    22

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,43 +1,77 @@
 import { ComponentType } from 'react'
 import Taro, { Component } from '@tarojs/taro'
-import { View,Swiper, SwiperItem } from '@tarojs/components'
+import { View,Swiper, SwiperItem,Image } from '@tarojs/components'
+import { AtGrid } from "taro-ui"
 import './index.scss'
-
 
 class Home extends Component<Component,any> {
   constructor(prop) {
     super(prop)
     this.state = {
-      bannerList: ['1','2','3']
+      bannerList: [require('../../images/banner1.png'),require('../../images/banner2.png')],
+      gridList: [],
+      gridTitleList: ['蔬菜豆制','肉禽蛋','水产海鲜','水果','乳品烘焙','米面粮油','方便速食','酒饮零食','快手菜','邀请有礼']
     }
   }
 
-  rendeItem() {
+  componentDidMount() {
+
+  }
+  componentWillMount() {
+    this.getGridImage()
+  }
+
+  getGridImage = () => {
+    let imgNameArray: Array<Object> = []
+    for (let index = 0; index < 10; index++) {
+      const ob = require(`../../images/grid${index+1}.png`)
+      imgNameArray.push({
+        image: ob,
+        value: this.state.gridTitleList[index]
+      })
+    }
+    this.setState({
+      gridList: imgNameArray
+    })
+  }
+
+  renderItem() {
     const {bannerList} = this.state;
-    const data = bannerList.map((num: string) => {
+    const data = bannerList.map((val: string) => {
       return (
-        <SwiperItem>
-          <View className="demo-text-1">
-            {num}
+        <SwiperItem className="demo-text-1" key='val'>
+          <View className="imgcon">
+            <Image className="imagecon" src={val} style='height: 130px;background: #fff;' />
           </View>
         </SwiperItem>
       )
     })
     return data;
   }
+  renderGridItem() {
+    const {gridList} = this.state
+    return <AtGrid
+    columnNum={5}
+    hasBorder={false}
+    data={gridList}
+    className="at-grid-style"
+  />
+  }
 
   render() {
     return (
       <View>
         <Swiper
-            className='test-h'
+            style='height: 130px;background: #fff;'
+            className='swiper-con'
             indicatorColor='#999'
             indicatorActiveColor='#333'
             circular
             indicatorDots
             autoplay>
-            {this.rendeItem()}
+            {this.renderItem()}
         </Swiper>
+        {this.renderGridItem()}
       </View>
     )
   }
